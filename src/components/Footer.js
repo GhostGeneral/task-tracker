b05ef23dc0ec504a47bb061b75fefc0ca4a82ec3
Ghost General <awotundun@gmail.com>
2022-02-13 10:18:44 +0100

    live server

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+      <Footer>
+          <p>Copyright &copy 2021</p>
+          <Link to='/about'>About</Link>
+      </Footer>
+  )
+};
+
+export default Footer;
